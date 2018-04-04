2ec54ae5cc77fd8c877a761553da725b509df25e
Catherine Arthur <catherine.arthur.hontos@gmail.com>
2018-04-03 20:05:19 -0700

    puuuush?

---
+++ StarHeart/Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+	public float speed = 1;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		Vector2 movement = new Vector2 (speed, 0);
+		rb2d.velocity = movement;
+	}
+
+}
---
+++ StarHeart/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+
+	public float speed = 1;
+
+	public GameObject bullet;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		Instantiate (bullet);
+	}
+	void Update()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		//rb2d.AddForce (movement * speed);
+		//rb2d.velocity = (movement * speed);
+		rb2d.position += (movement * speed);
+	}
+}
